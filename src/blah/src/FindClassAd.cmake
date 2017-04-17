5249b707d67ed378d533f6d29b740183fa1cce47
Derek Weitzel <dweitzel@cse.unl.edu>
2017-04-17 21:41:54 +0000

    Building and installing of BLAHP working.

--- src/blah/src/FindClassAd.cmake
+++ src/blah/src/FindClassAd.cmake
@@ -26,10 +26,13 @@ set(ClassAd_INCLUDE_DIR_MESSAGE "Set the ClassAd_INCLUDE_DIR cmake cache entry t
 set(ClassAd_LIBRARY_PATH_DESCRIPTION "top-level directory containing the Condor ClassAd libraries.")
 set(ClassAd_LIBRARY_DIR_MESSAGE "Set the ClassAd_LIBRARY_DIR cmake cache entry to the ${ClassAd_LIBRARY_PATH_DESCRIPTION}")
 
+message(WARNING, "Classads directory is ${CONDOR_SOURCE_DIR}/src/classad")
+
 find_path(ClassAd_INCLUDE_DIR
   NAMES classad_distribution.h
   PATHS
    # Look in other places.
+   ${CONDOR_SOURCE_DIR}/src/classad
    ${ClassAd_ROOT_DIRECTORIES}
   PATH_SUFFIXES
     classad
@@ -41,72 +44,72 @@ find_path(ClassAd_INCLUDE_DIR
 message(STATUS "ClassAd_INCLUDE_DIR == " ${ClassAd_INCLUDE_DIR})
 
 # The ClassAd library (should have namespaces enabled).
-set (ClassAd_LIBRARY_TO_FIND classad_ns)
+#set (ClassAd_LIBRARY_TO_FIND classad_ns)
 
 # Setting some more prefixes for the library
-set (ClassAd_LIB_PREFIX "")
-if ( WIN32 )
-  set (ClassAd_LIB_PREFIX ${ClassAd_LIB_PREFIX} "lib")
-  set ( ClassAd_LIBRARY_TO_FIND ${ClassAd_LIB_PREFIX}${ClassAd_LIBRARY_TO_FIND})
-endif()
-
-find_library( ClassAd_LIBRARY
- NAMES ${ClassAd_LIBRARY_TO_FIND}
- PATHS
-   ${ClassAd_LIBRARY_DIR}
- PATH_SUFFIXES
-   lib
-)
-
-get_filename_component(ClassAd_LIBRARY_DIR ${ClassAd_LIBRARY} PATH)
-message(STATUS "ClassAd_LIBRARY == " ${ClassAd_LIBRARY})
-
-# sample Classad source code to test
-set(ClassAd_CXX_TEST_SOURCE 
-"
-#include <classad_distribution.h>
-classad::ClassAd ad;
-classad::ClassAdParser parser;
-
-int
-main(int argc, char *argv[])
-{
-}
-")
-
-set(ClassAd_CXX_FLAG_CANDIDATES
-  "-DWANT_NAMESPACES"
-  "-DWANT_NAMESPACES -DWANT_CLASSAD_NAMESPACE"
-)
-
-# check cxx compiler
-foreach(FLAG ${ClassAd_CXX_FLAG_CANDIDATES})
-  set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
-  set(SAFE_CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES}")
-  set(SAFE_CMAKE_REQUIRED_INCLUDES "${CMAKE_REQUIRED_INCLUDES}")
-  set(CMAKE_REQUIRED_FLAGS "${FLAG}")
-  set(CMAKE_REQUIRED_LIBRARIES "${ClassAd_LIBRARY}")
-  set(CMAKE_REQUIRED_INCLUDES "${ClassAd_INCLUDE_DIR}")
-  unset(ClassAd_FLAG_DETECTED CACHE)
-  message(STATUS "Try Classad CXX flag = [${FLAG}] (library = [${ClassAd_LIBRARY}])")
-  check_cxx_source_compiles("${ClassAd_CXX_TEST_SOURCE}" ClassAd_FLAG_DETECTED)
-  set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
-  set(CMAKE_REQUIRED_LIBRARIES "${SAFE_CMAKE_REQUIRED_LIBRARIES}")
-  set(CMAKE_REQUIRED_INCLUDES "${SAFE_CMAKE_REQUIRED_INCLUDES}")
-  if(ClassAd_FLAG_DETECTED)
-    set(ClassAd_CXX_FLAGS_INTERNAL "${FLAG}")
-    break()
-  endif(ClassAd_FLAG_DETECTED)
-endforeach(FLAG ${ClassAd_CXX_FLAG_CANDIDATES})
-
-set(ClassAd_CXX_FLAGS "${ClassAd_CXX_FLAGS_INTERNAL}"
-  CACHE STRING "C++ compiler flags for use of the Condor Classad  library")
-message(STATUS "ClassAd_CXX_FLAGS == " ${ClassAd_CXX_FLAGS})
-# handle the standard arguments for find_package
-find_package_handle_standard_args(ClassAd DEFAULT_MSG 
-  ClassAd_LIBRARY ClassAd_INCLUDE_DIR)
-
-mark_as_advanced(
-  ClassAd_CXX_FLAGS
-  ClassAd_LIBRARY
-)
+#set (ClassAd_LIB_PREFIX "")
+#if ( WIN32 )
+#  set (ClassAd_LIB_PREFIX ${ClassAd_LIB_PREFIX} "lib")
+#  set ( ClassAd_LIBRARY_TO_FIND ${ClassAd_LIB_PREFIX}${ClassAd_LIBRARY_TO_FIND})
+#endif()
+#
+#find_library( ClassAd_LIBRARY
+# NAMES ${ClassAd_LIBRARY_TO_FIND}
+# PATHS
+#   ${ClassAd_LIBRARY_DIR}
+# PATH_SUFFIXES
+#   lib
+#)
+#
+#get_filename_component(ClassAd_LIBRARY_DIR ${ClassAd_LIBRARY} PATH)
+#message(STATUS "ClassAd_LIBRARY == " ${ClassAd_LIBRARY})
+#
+## sample Classad source code to test
+#set(ClassAd_CXX_TEST_SOURCE 
+#"
+##include <classad_distribution.h>
+#classad::ClassAd ad;
+#classad::ClassAdParser parser;
+#
+#int
+#main(int argc, char *argv[])
+#{
+#}
+#")
+#
+#set(ClassAd_CXX_FLAG_CANDIDATES
+#  "-DWANT_NAMESPACES"
+#  "-DWANT_NAMESPACES -DWANT_CLASSAD_NAMESPACE"
+#)
+#
+## check cxx compiler
+#foreach(FLAG ${ClassAd_CXX_FLAG_CANDIDATES})
+#  set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+#  set(SAFE_CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES}")
+#  set(SAFE_CMAKE_REQUIRED_INCLUDES "${CMAKE_REQUIRED_INCLUDES}")
+#  set(CMAKE_REQUIRED_FLAGS "${FLAG}")
+#  set(CMAKE_REQUIRED_LIBRARIES "${ClassAd_LIBRARY}")
+#  set(CMAKE_REQUIRED_INCLUDES "${ClassAd_INCLUDE_DIR}")
+#  unset(ClassAd_FLAG_DETECTED CACHE)
+#  message(STATUS "Try Classad CXX flag = [${FLAG}] (library = [${ClassAd_LIBRARY}])")
+#  check_cxx_source_compiles("${ClassAd_CXX_TEST_SOURCE}" ClassAd_FLAG_DETECTED)
+#  set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
+#  set(CMAKE_REQUIRED_LIBRARIES "${SAFE_CMAKE_REQUIRED_LIBRARIES}")
+#  set(CMAKE_REQUIRED_INCLUDES "${SAFE_CMAKE_REQUIRED_INCLUDES}")
+#  if(ClassAd_FLAG_DETECTED)
+#    set(ClassAd_CXX_FLAGS_INTERNAL "${FLAG}")
+#    break()
+#  endif(ClassAd_FLAG_DETECTED)
+#endforeach(FLAG ${ClassAd_CXX_FLAG_CANDIDATES})
+#
+#set(ClassAd_CXX_FLAGS "${ClassAd_CXX_FLAGS_INTERNAL}"
+#  CACHE STRING "C++ compiler flags for use of the Condor Classad  library")
+#message(STATUS "ClassAd_CXX_FLAGS == " ${ClassAd_CXX_FLAGS})
+## handle the standard arguments for find_package
+#find_package_handle_standard_args(ClassAd DEFAULT_MSG 
+#  ClassAd_LIBRARY ClassAd_INCLUDE_DIR)
+#
+#mark_as_advanced(
+#  ClassAd_CXX_FLAGS
+#  ClassAd_LIBRARY
+#)
